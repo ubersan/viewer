f27eec7003228a15dafa96f0f57b61bc702b3866
Sandro Huber <sandrohuber89@hotmail.com>
2019-04-14 12:01:01 +0200

    Create swap chain.

--- include/viewer.hpp
+++ include/viewer.hpp
@@ -1,17 +1,30 @@
 #define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
 
+#include <vector>
+
 class Viewer {
 public:
   ~Viewer();
 
   void run();
 private:
+  uint32_t width{640};
+  uint32_t height{480};
+
   GLFWwindow* window;
 
   VkInstance vkInstance;
   VkPhysicalDevice physicalDevice;
   VkDevice logicalDevice;
+  VkQueue graphicsQueue;
+  VkSurfaceKHR surface;
+  VkQueue presentationQueue;
+  VkSwapchainKHR swapChain;
+
+  std::vector<const char*> logicalDeviceExtensions{
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+  };
 
   static void exit_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 };
--- source/viewer.cpp
+++ source/viewer.cpp
@@ -1,10 +1,11 @@
 #include "viewer.hpp"
 
 #include <iostream>
-#include <vector>
 
 Viewer::~Viewer() {
+  vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
   vkDestroyDevice(logicalDevice, nullptr);
+  vkDestroySurfaceKHR(vkInstance, surface, nullptr);
   vkDestroyInstance(vkInstance, nullptr);
 
   glfwTerminate();
@@ -14,11 +15,11 @@ Viewer::~Viewer() {
 void Viewer::run() {
   glfwInit();
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  window = glfwCreateWindow(640, 480, "3D Viewer", nullptr, nullptr);
+  window = glfwCreateWindow(width, height, "3D Viewer", nullptr, nullptr);
 
   glfwSetKeyCallback(window, exit_key_callback); 
 
-  VkApplicationInfo applicationInfo = {
+  VkApplicationInfo applicationInfo{
     .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
     .pApplicationName = "3D Viewer",
     .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
@@ -30,7 +31,7 @@ void Viewer::run() {
   auto glfwExtensionCount = uint32_t{0};
   auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-  VkInstanceCreateInfo instanceCreateInfo = {
+  VkInstanceCreateInfo instanceCreateInfo{
     .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
     .pApplicationInfo = &applicationInfo,
     .enabledExtensionCount = glfwExtensionCount,
@@ -41,6 +42,10 @@ void Viewer::run() {
     throw std::runtime_error("Could not create vulkan instance");
   }
 
+  if (glfwCreateWindowSurface(vkInstance, window, nullptr, &surface) != VK_SUCCESS) {
+    throw std::runtime_error("Could not create window surface");
+  }
+
   auto pyhsicalDeviceCount = uint32_t{0};
   vkEnumeratePhysicalDevices(vkInstance, &pyhsicalDeviceCount, nullptr);
   std::vector<VkPhysicalDevice> physicalDevices(pyhsicalDeviceCount);
@@ -48,7 +53,7 @@ void Viewer::run() {
   physicalDevice = physicalDevices[0];
 
   auto queuePriority = float{1.0f};
-  VkDeviceQueueCreateInfo logicalDeviceQueueCreateInfo = {
+  VkDeviceQueueCreateInfo logicalDeviceQueueCreateInfo{
     .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
     .queueFamilyIndex = 0,
     .queueCount = 1,
@@ -56,15 +61,48 @@ void Viewer::run() {
   };
 
   VkPhysicalDeviceFeatures logicalDeviceFeatures{};
-  VkDeviceCreateInfo logicalDeviceCreateInfo = {
+  VkDeviceCreateInfo logicalDeviceCreateInfo{
     .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
     .queueCreateInfoCount = 1,
     .pQueueCreateInfos = &logicalDeviceQueueCreateInfo,
+    .enabledExtensionCount = 1,
+    .ppEnabledExtensionNames = logicalDeviceExtensions.data(),
     .pEnabledFeatures = &logicalDeviceFeatures
   };
 
   if (vkCreateDevice(physicalDevice, &logicalDeviceCreateInfo, nullptr, &logicalDevice) != VK_SUCCESS) {
     throw std::runtime_error("Could not create logical device");
+  }
+
+  vkGetDeviceQueue(logicalDevice, 0, 0, &graphicsQueue);
+  vkGetDeviceQueue(logicalDevice, 0, 0, &presentationQueue);
+
+  VkSurfaceFormatKHR surfaceFormat{VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR};
+  VkPresentModeKHR presentationMode{VK_PRESENT_MODE_FIFO_KHR};
+  VkExtent2D swapExtent{width, height};
+
+  VkSurfaceCapabilitiesKHR surfaceCapabilities;
+  vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfaceCapabilities);
+
+  VkSwapchainCreateInfoKHR swapChainCreateInfo{
+    .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+    .surface = surface,
+    .minImageCount = surfaceCapabilities.minImageCount + 1,
+    .imageFormat = surfaceFormat.format,
+    .imageColorSpace = surfaceFormat.colorSpace,
+    .imageExtent = swapExtent,
+    .imageArrayLayers = 1,
+    .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+    .imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
+    .preTransform = surfaceCapabilities.currentTransform,
+    .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+    .presentMode = presentationMode,
+    .clipped = VK_TRUE,
+    .oldSwapchain = VK_NULL_HANDLE
+  };
+
+  if (vkCreateSwapchainKHR(logicalDevice, &swapChainCreateInfo, nullptr, &swapChain) != VK_SUCCESS) {
+    throw std::runtime_error("Could not create swap chain");
   }
 
   while (!glfwWindowShouldClose(window)) {
